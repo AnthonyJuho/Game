ad0a767c3bf6e4f26aba5555b41d4b04705149e8
AnthonyJuho <112909624+AnthonyJuho@users.noreply.github.com>
2022-10-10 22:29:27 +0900

    click test

--- js/javascript.js
+++ js/javascript.js
@@ -1,10 +1,3 @@
-var content = document.getElementsByClassName('content');
-
-StartScreen();
-
-function StartScreen(){
-    var startbutton = document.createElement("input");
-    startbutton.type = "button";
-    startbutton.className = "startbutton";
-    content.prepend(startbutton);
-}
+document.addEventListener("click", e=>{
+    alert("click!");
+});
