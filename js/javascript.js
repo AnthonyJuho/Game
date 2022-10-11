4a5b7f26bb016374601f2e1716c99cf50a8b4a0a
AnthonyJuho <112909624+AnthonyJuho@users.noreply.github.com>
2022-10-11 22:51:10 +0900

    Add files via upload

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,22 @@
-document.addEventListener("click", e=>{
-    alert("click!");
+var start = document.getElementById('start');
+
+var isgaming = false;
+
+
+start.addEventListener("click", e=>{
+    isgaming = true;
+    Start();
 });
+
+function Start() {
+    document.getElementById('description').innerHTML = "";
+
+    //Count Down
+    for(let time = 3; time >=1; time--){
+        setTimeout(function(){
+            // console.log(time);
+            document.getElementById('description').innerHTML = '<div id="count">'+time+"<div>";
+        }, 1000*(4-time));
+    }
+
+}
